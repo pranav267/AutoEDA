bb054ca7a399d66db42ee1e35474f5068e5acab2
pranav267 <71278573+pranav267@users.noreply.github.com>
2021-05-28 13:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from eda_script import eda
 
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title='AutoEDA', page_icon='icon.png')
 
 eda()
